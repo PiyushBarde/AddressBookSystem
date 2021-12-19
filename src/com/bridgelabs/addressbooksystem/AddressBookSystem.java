e25b5a1a4af646ccd87e0a672ca639f1dda15d1f
PiyushBarde <piyushbarde1@gmail.com>
2021-12-19 17:10:31 +0530

    ADDED: adbility to creat contact in addressbook

--- src/com/bridgelabs/addressbooksystem/AddressBookSystem.java
+++ src/com/bridgelabs/addressbooksystem/AddressBookSystem.java
@@ -1,10 +1,31 @@
 package com.bridgelabs.addressbooksystem;
-
+import java.util.Scanner;
 public class AddressBookSystem {
+    static class AddressBook {
+          String firstName, lastName, address, city, state;
+          int zipCode;
+          long phoneNumber;
 
-	public static void main(String[] args) {
-		System.out.println("Welcome To The Address Book Program");
-
-	}
-
+             AddressBook() {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter first Name = ");
+            firstName = sc.nextLine();
+            System.out.println("Enter Last Name = ");
+            lastName = sc.nextLine();
+            System.out.println("Enter address = ");
+            address = sc.nextLine();
+            System.out.println("vEnter city = ");
+            city = sc.nextLine();
+            System.out.println("Enter state = ");
+            state = sc.nextLine();
+            System.out.println("Enter zipCode = ");
+            zipCode = sc.nextInt();
+            System.out.println("Enter phoneNumber = ");
+            phoneNumber = sc.nextLong();
+        }
+    }
+    
+    public static void main(String[] args) {
+    	AddressBook person1 = new AddressBook();
+    }
 }
